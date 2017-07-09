ff27eb58ae6e696d4293f58eac1553c925cce8c9
Matt Lubel <matt_lubel@hotmail.com>
2017-07-09 15:24:46 +0300

    connect to db before starting the server #35

--- src/start.js
+++ src/start.js
@@ -1,10 +1,26 @@
+const mongoose = require('mongoose')
 const server = require('./server.js')
+
+// connect to the db
+require('../db/connect.js')
+const db = mongoose.connection
 
 const port = process.env.PORT || 3000
 
-server.listen(port, err => {
-  if (err) {
-    throw err
-  }
-  console.log(`Server listening on port ${port}`)
+db.on('error', err => console.log('connection error: ' + err))
+
+db.once('open', () => {
+  // we're connected to the database
+  server.listen(port, err => {
+    if (err) {
+      throw err
+    }
+    console.log(`Server listening on port ${port}`)
+
+    const User = require('../db/models/User.js')
+    User.find((err, users) => {
+      if (err) throw err
+      console.log(users)
+    })
+  })
 })
