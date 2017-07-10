1bf7dcccf517f8940dc32086367dc5ff4a4a454a
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-10 15:32:16 +0300

    rearrange order in schema

--- db/models/User.js
+++ db/models/User.js
@@ -4,15 +4,15 @@ const roles = ['BASIC', 'ADMIN', 'SUPER']
 
 const userSchema = mongoose.Schema(
   {
-    isPublic: Boolean,
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     name: { type: String, required: true },
-    organisationName: { type: String, required: false },
-    organisationDescription: { type: String, required: false },
     email: { type: String, required: true },
-    role: { type: String, enum: roles, required: true},
-    imageUrl: { type: String, required: false }
+    role: { type: String, enum: roles, required: true },
+    isPublic: { type: Boolean, required: true, default: true },
+    organisationName: String,
+    organisationDescription: String,
+    imageUrl: String
   },
   {
     timestamps: true
