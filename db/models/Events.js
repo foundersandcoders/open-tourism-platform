9f532c71718072c2f0333f3c2e40385758d89c83
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-10 16:37:16 +0300

    add events schema and model
    relates #41

---
+++ db/models/Events.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const categories = ['music', 'dining']
+const accessibilityOptions = ['braille menu', 'wheelchair-friendly']
+
+const eventSchema = mongoose.Schema(
+  {
+    ownerId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    name: { type: String, required: true },
+    category: { type: [String], enum: categories, required: true },
+    location: {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+    accessibilityOptions: { type: [String], enum: accessibilityOptions, required: false },
+    startTime: Date,
+    endTime: Date,
+    description: String,
+    cost: String
+    imageUrl: String
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Event', eventSchema)
