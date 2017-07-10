0185077bf7f3c0b133896d9affac85dc0e65658e
Matt Lubel <matt_lubel@hotmail.com>
2017-07-10 16:33:51 +0300

    create places model #40

---
+++ db/models/Place.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+// placeholders for now
+const categories = ['restaurant, cafe']
+const accesibilityOptions = ['braille menu', 'wheelchair-friendly']
+
+const placeSchema = mongoose.Schema(
+  {
+    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: String,
+    description: String,
+    address: String,
+    lat: Number,
+    lng: Number,
+    category: { type: [String], enum: categories },
+    accessibilityOptions: { type: [String], enum: accesibilityOptions },
+    openingHours: ,
+    imageUrl: String,
+    website: String,
+    phone: String,
+    email: String
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Place', placeSchema)
