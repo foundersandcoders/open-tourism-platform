bedc0529391a81a20ce530b5cd227bfa8719df18
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-12 13:15:19 +0300

    test db connection #47

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,22 @@
 require('../config.js')
 const test = require('tape')
-// const dbConnection = require('../db/connect.js')
+const dbConnection = require('../db/connect.js')
 // const server = require('../src/server.js')
 // const supertest = require('supertest')
-// ^For when we start testing, require in server and supertest
-
-test('Check tape is working', t => {
+// const Users = require('../db/models/User.js')
+// ^ will use these in the futue
+test('Check tape is working before db connection', t => {
   t.equal(1, 1, 'One is one')
   t.end()
+})
+
+test('Check db connection', t => {
+  dbConnection.once('open', () => {
+    t.pass('Connected to database')
+    t.end()
+  })
+})
+
+test.onFinish(() => {
+  dbConnection.close()
 })
