af1c356d27de879c06672235911a9069f181208c
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-13 16:25:02 +0300

    remove console log and unneeded comments

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 if (process.env.NODE_ENV !== 'production' && !process.env.CI) {
   require('dotenv').config()
   if (process.env.NODE_ENV === 'test') {
-    // set environment variables for test environment
     process.env.MONGODB_URI = process.env.MONGODB_URI_TEST
   } else if (process.env.NODE_ENV === 'development') {
-    // set environment variables for dev environment
     process.env.MONGODB_URI = process.env.MONGODB_URI_DEV
   }
 }
-
-console.log(process.env.MONGODB_URI)
