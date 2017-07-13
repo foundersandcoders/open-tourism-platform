a822e9a629abb6579787dc9a88ac8f2b433a16c8
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-13 11:43:11 +0300

    allow for CI in config.js #47

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' && !process.env.CI) {
   require('dotenv').config()
-  switch (process.env.NODE_ENV) {
-    case 'test':
-      process.env.MONGODB_URI = process.env.MONGODB_URI_TEST
-      break
-    case 'development':
-      process.env.MONGODB_URI = process.env.MONGODB_URI_DEV
-      break
+  if (process.env.NODE_ENV === 'test') {
+    // set environment variables for test environment
+    process.env.MONGODB_URI = process.env.MONGODB_URI_TEST
+  } else if (process.env.NODE_ENV === 'development') {
+    // set environment variables for dev environment
+    process.env.MONGODB_URI = process.env.MONGODB_URI_DEV
   }
 }
+
+console.log(process.env.MONGODB_URI)
