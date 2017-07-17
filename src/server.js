68251e383946405bcbb473b3d82fb33f23b189f4
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-17 10:15:45 +0300

    add body parser
    #48

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = require('./routes')
+const bodyParser = require('body-parser')
 
 const server = express()
+
+server.use(bodyParser.urlencoded())
+server.use(bodyParser.json())
 
 server.use(router)
 
