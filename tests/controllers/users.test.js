0aafd57e3f52c926f40d86e1ceb53f6d9ba76897
Matt Lubel <matt_lubel@hotmail.com>
2017-07-18 10:51:10 +0300

    change constant user variable names

--- tests/controllers/users.test.js
+++ tests/controllers/users.test.js
@@ -3,7 +3,7 @@ const supertest = require('supertest')
 const server = require('../../src/server.js')
 const User = require('../../src/models/User.js')
 const { dropCollectionAndEnd } = require('../helpers/index.js')
-const testData = require('../fixtures/users.json')
+const { validUser1, validUser2 } = require('../fixtures/users.json')
 
 tape('test /users when nothing in database', (t) => {
   supertest(server)
@@ -19,7 +19,7 @@ tape('test /users when nothing in database', (t) => {
 
 tape('test /users when one user in database', t => {
   // add mock user to database using mongoose ORM
-  const mockUser = new User(testData.a_user)
+  const mockUser = new User(validUser1)
   mockUser.save()
     .then(() => {
       supertest(server)
@@ -31,7 +31,7 @@ tape('test /users when one user in database', t => {
           // check our get path returns that user correctly
           t.ok(res.body.length, 'response body should have length property')
           t.equal(res.body.length, 1, 'response body should be an array with length 1')
-          t.equal(res.body[0].username, testData.a_user.username, 'correct username is returned')
+          t.equal(res.body[0].username, validUser1.username, 'correct username is returned')
           dropCollectionAndEnd(User, t)
         })
     })
@@ -39,7 +39,7 @@ tape('test /users when one user in database', t => {
 })
 
 tape('test /users GET returns longer list', t => {
-  User.create(testData.a_user, testData.another_user)
+  User.create(validUser1, validUser2)
     .then(() => {
       supertest(server)
         .get('/users')
@@ -49,8 +49,8 @@ tape('test /users GET returns longer list', t => {
           if (err) t.fail(err)
           // check our get path returns that user correctly
           t.equal(res.body.length, 2, 'response body should be an array with length 1')
-          t.ok(res.body.map((user) => user.username).includes(testData.a_user.username), 'mattlub has been added')
-          t.ok(res.body.map((user) => user.username).includes(testData.another_user.username), 'm4v15 has been added')
+          t.ok(res.body.map((user) => user.username).includes(validUser1.username), 'mattlub has been added')
+          t.ok(res.body.map((user) => user.username).includes(validUser2.username), 'm4v15 has been added')
           dropCollectionAndEnd(User, t)
         })
     })
