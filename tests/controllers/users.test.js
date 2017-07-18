809dacebce9d70e4f597471d1a66b2bae20c7712
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-18 15:05:20 +0300

    add tests for invalid user

--- tests/controllers/users.test.js
+++ tests/controllers/users.test.js
@@ -3,7 +3,7 @@ const supertest = require('supertest')
 const server = require('../../src/server.js')
 const User = require('../../src/models/User.js')
 const { dropCollectionAndEnd } = require('../helpers/index.js')
-const { validUser1, validUser2 } = require('../fixtures/users.json')
+const { validUser1, validUser2, invalidUser1 } = require('../fixtures/users.json')
 
 // Tests for: GET /users
 tape('test /users when nothing in database', (t) => {
@@ -45,11 +45,36 @@ tape('testing adding user using POST requests to /users route', t => {
   supertest(server)
     .post('/users')
     .send(validUser1)
-    .expect(200)
+    .expect(201)
     .expect('Content-Type', /json/)
     .end((err, res) => {
       if (err) t.fail(err)
-      t.deepEqual(res.body, validUser1, 'Respond with object that was added')
+      t.equal(res.body.username, validUser1.username, 'Correct object is added')
+      t.ok(res.body._id && res.body.createdAt && res.body.updatedAt, 'id and timestamp fields added')
+      // Now check whether it is in the database
+      User.findById(res.body._id)
+        .then(user => {
+          t.equal(user.email, res.body.email, 'User is in the database')
+          dropCollectionAndEnd(User, t)
+        })
+        .catch(err => {
+          t.fail(err)
+          dropCollectionAndEnd(User, t)
+        })
+    })
+})
+
+tape('testing adding user using POST requests to /users route', t => {
+  supertest(server)
+    .post('/users')
+    .send(invalidUser1)
+    .expect(500)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) t.fail(err)
+      t.ok(res.body.message, 'A message is sent back')
+      t.ok(res.body.message.includes('Database error'), 'Correct message is sent back')
+
       dropCollectionAndEnd(User, t)
     })
 })
