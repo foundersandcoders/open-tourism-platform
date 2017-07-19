490eebb874887bc961a2b451a44da079294e6e3a
Matt Lubel <matt_lubel@hotmail.com>
2017-07-19 17:22:43 +0300

    place.getAll #49

--- src/controllers/place.js
+++ src/controllers/place.js
@@ -3,7 +3,16 @@ const Place = require('../models/Place')
 const placeController = module.exports = {}
 
 placeController.getAll = (req, res) => {
-
+  // sends back array of places, filtered by queries
+  // status codes: 200 (success)
+  Place.find(req.query)
+    .then(places => {
+      res.send(places)
+    })
+    .catch(err => {
+      const errorObj = { message: `Database error: ${err.message}` }
+      res.status(500).send(errorObj)
+    })
 }
 
 placeController.getById = (req, res) => {
--- tests/controllers/place.test.js
+++ tests/controllers/place.test.js
@@ -6,6 +6,46 @@ const { dropCollectionAndEnd } = require('../helpers/index.js')
 const { validPlace1, validPlace2, invalidPlace1 } = require('../fixtures/places.json')
 
 // Tests for: GET /places
+tape('GET /places when nothing in database', (t) => {
+  supertest(server)
+    .get('/places')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) t.fail(err)
+      t.equal(res.body.length, 0, 'should initially return empty array')
+      dropCollectionAndEnd(Place, t)
+    })
+})
+
+tape('GET /places, with and without query parameters', t => {
+  Place.create(validPlace1, validPlace2)
+    .then(() => {
+      supertest(server)
+        .get('/places')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) t.fail(err)
+          // check our get path returns that place correctly
+          t.equal(res.body.length, 2, 'response body should be an array with length 2')
+          t.ok(res.body.map(place => place.name).includes(validPlace1.name), 'first place has been added')
+          t.ok(res.body.map(place => place.name).includes(validPlace2.name), 'second place has been added')
+        })
+      supertest(server)
+        .get('/places?name=Basilica')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) t.fail(err)
+          // check our get path returns that place correctly
+          t.equal(res.body.length, 1, 'filtered response body should be an array with length 1')
+          t.equal(res.body[0].name, 'Basilica', 'results should be filtered correctly by url query parameters')
+          dropCollectionAndEnd(Place, t)
+        })
+    })
+    .catch(err => t.end(err))
+})
 
 // Tests for: GET /places/:id
 
