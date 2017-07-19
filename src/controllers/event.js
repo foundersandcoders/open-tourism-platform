6b5fe7d30ec8b16bca87cdb9099875908eb01e04
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-19 15:51:24 +0300

    add tests and functionality for eventController.getAll

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -3,7 +3,16 @@ const Event = require('../models/Event')
 const eventController = module.exports = {}
 
 eventController.getAll = (req, res) => {
-
+  // sends back array of events, filtered by queries
+  // status codes: 200 (success)
+  Event.find(req.query)
+    .then(events => {
+      res.send(events)
+    })
+    .catch(err => {
+      const errorObj = { message: `Database error: ${err.message}` }
+      res.status(500).send(errorObj)
+    })
 }
 
 eventController.getById = (req, res) => {
--- tests/controllers/event.test.js
+++ tests/controllers/event.test.js
@@ -3,9 +3,51 @@ const supertest = require('supertest')
 const server = require('../../src/server.js')
 const Event = require('../../src/models/Event.js')
 const { dropCollectionAndEnd } = require('../helpers/index.js')
-const { validEvent1, validEvent2, invalidEvent1 } = require('../fixtures/events.json')
+const { validEvent1, validEvent2, validEvent3, invalidEvent1 } = require('../fixtures/events.json')
+console.log(validEvent2)
 
 // Tests for: GET /events
+tape('GET /events when nothing in database', (t) => {
+  supertest(server)
+    .get('/events')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) t.fail(err)
+      t.equal(res.body.length, 0, 'should initially return empty array')
+      dropCollectionAndEnd(Event, t)
+    })
+})
+
+tape('GET /events, with and without query parameters', t => {
+  Event.create(validEvent1, validEvent2, validEvent3)
+    .then(() => {
+      supertest(server)
+        .get('/events')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) t.fail(err)
+          // check our get path returns that user correctly
+          t.equal(res.body.length, 3, 'response body should be an array with length 3')
+          t.ok(res.body.map(event => event.name).includes(validEvent1.name), 'first event has been added')
+          t.ok(res.body.map(event => event.name).includes(validEvent2.name), 'second event has been added')
+          t.ok(res.body.map(event => event.name).includes(validEvent3.name), 'third event has been added')
+        })
+      supertest(server)
+        .get('/events?category=dining')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) t.fail(err)
+          // check our get path returns that user correctly
+          t.equal(res.body.length, 2, 'query response body should be an array with length 2')
+          t.equal(res.body[0].name, validEvent2.name, 'results should be filtered correctly by url query parameters')
+          dropCollectionAndEnd(Event, t)
+        })
+    })
+    .catch(err => t.end(err))
+})
 
 // Tests for: GET /events/:id
 
