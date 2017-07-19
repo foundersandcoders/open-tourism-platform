23c9ed5dc3dd07c89992588f5162329f075f98a9
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-19 17:53:36 +0300

    fix indentation in index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,7 +6,8 @@ dbConnection.once('open', () => {
   require('./controllers/user.test.js')
   require('./controllers/place.test.js')
   require('./controllers/event.test.js')
-  require('./controllers/product.test.js')  tape.onFinish(() => {
+  require('./controllers/product.test.js')
+  tape.onFinish(() => {
     dbConnection.close()
   })
 })
