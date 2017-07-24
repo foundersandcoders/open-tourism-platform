3c4568f5047f6ef29d2b28d225454863152f25c8
Matt Lubel <matt_lubel@hotmail.com>
2017-07-24 15:24:44 +0300

    add findByIdAndUpdateOrError to User schema #71

--- src/models/User.js
+++ src/models/User.js
@@ -19,20 +19,40 @@ const userSchema = mongoose.Schema(
   }
 )
 
-// Our own static method in order to make it throw an error when it doesn't find something matching a valid ID
-userSchema.statics.findByIdOrError = function (id) {
+const customDbError = message => {
+  const err = new Error(message)
+  err.name = 'CustomDbError'
+  return err
+}
+
+const findByIdOrError = function (id) {
   return new Promise((resolve, reject) => {
     this.findById(id)
       .then(user => {
         if (!user) {
-          const noIdErr = new Error('No document matching that id')
-          noIdErr.name = 'Custom DB error'
-          return reject(noIdErr)
+          return reject(customDbError('No document matching that id'))
         }
         resolve(user)
       })
       .catch(reject)
   })
 }
+
+const findByIdAndUpdateOrError = function (id, data, options) {
+  return new Promise((resolve, reject) => {
+    this.findByIdAndUpdate(id, data, options)
+      .then(user => {
+        if (!user) {
+          return reject(customDbError('No document matching that id'))
+        }
+        resolve(user)
+      })
+      .catch(reject)
+  })
+}
+
+// Our own static method in order to make it throw an error when it doesn't find something matching a valid ID
+userSchema.statics.findByIdOrError = findByIdOrError
+userSchema.statics.findByIdAndUpdateOrError = findByIdAndUpdateOrError
 
 module.exports = mongoose.model('User', userSchema)
