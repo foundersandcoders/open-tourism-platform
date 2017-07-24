dec7108107e41663b95278876b1ed3c2f8d3509c
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-24 13:00:31 +0300

    allow multilanguage support in User schema

--- src/models/User.js
+++ src/models/User.js
@@ -2,17 +2,24 @@ const mongoose = require('mongoose')
 
 const { roles } = require('./constants.json')
 
+const UserTranslatedFieldsSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    organisationName: String,
+    organisationDescription: String
+  }
+)
+
 const userSchema = mongoose.Schema(
   {
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
-    name: { type: String, required: true },
     email: { type: String, required: true },
     role: { type: String, enum: roles, required: true },
     isPublic: { type: Boolean, required: true, default: true },
-    organisationName: String,
-    organisationDescription: String,
-    imageUrl: String
+    imageUrl: String,
+    en: UserTranslatedFieldsSchema,
+    ar: UserTranslatedFieldsSchema
   },
   {
     timestamps: true
