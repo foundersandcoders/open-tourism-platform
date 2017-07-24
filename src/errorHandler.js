b527eccf3e278206900bd833eae09e7207095682
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-24 10:53:45 +0300

    deal with some of the expected errors #71

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -4,11 +4,18 @@ errorHandlers.mongoError = (err, req, res, next) => {
   if (err.name !== 'MongoError') {
     return next(err)
   }
+  console.log('mongo other error')
   return res.boom.badImplementation('An internal server error occurred')
 }
 
 errorHandlers.mongooseError = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
-    return res.boom.badRequest('Validation Failed', err.ValidationError)
+    return res.boom.badRequest('Validation Failed', err.ValidationError) // adding more info as data (See boom docs)
+  } else if (err.name === 'CastError') {
+    return res.boom.badRequest('Invalid id', err.CastError)
+  } else if (err.message === 'No document matching that id') {
+    return res.boom.notFound(err.message)
   }
+  console.log('mongoose other error')
+  return res.boom.badImplementation('An internal server error occurred')
 }
