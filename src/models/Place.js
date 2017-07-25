f8c7081e3f863bb65dcb6b3b5f7f98cfa2b390d3
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-25 16:07:23 +0300

    add places multilanguage support

--- src/models/Place.js
+++ src/models/Place.js
@@ -1,25 +1,35 @@
 const mongoose = require('mongoose')
 
 const { placeCategories, accessibilityOptions } = require('./constants.json')
+const { customRequireValidator } = require('../db/utils')
+
+const placeTranslatedFieldsSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: String,
+    address: String,
+    openingHours: String
+  }
+)
 
 const placeSchema = mongoose.Schema(
   {
     ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    name: { type: String, required: true },
-    description: String,
-    address: String,
     location: { type: [Number], index: '2dsphere' },
     category: [{ type: String, enum: placeCategories }],
     accessibilityOptions: [{ type: String, enum: accessibilityOptions }],
-    openingHours: String,
     imageUrl: String,
     website: String,
     phone: String,
-    email: String
+    email: String,
+    en: placeTranslatedFieldsSchema,
+    ar: placeTranslatedFieldsSchema
   },
   {
     timestamps: true
   }
 )
+
+placeSchema.pre('validate', customRequireValidator)
 
 module.exports = mongoose.model('Place', placeSchema)
