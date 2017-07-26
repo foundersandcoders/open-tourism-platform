97953191c92129a496ae6605cb3e1dde96f3ba37
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-26 11:48:15 +0300

    finish testing custom error handler

--- tests/dbErrorHandlers.test.js
+++ tests/dbErrorHandlers.test.js
@@ -21,7 +21,7 @@ tape('dbErrorHandlers.custom with non custom error', t => {
   t.end()
 })
 
-tape('dbErrorHandlers.custom with id not found error', t => {
+tape('dbErrorHandlers.custom with GET_ID_NOT_FOUND', t => {
   // construct arguments
   const testError = createCustomDbError(errMessages.GET_ID_NOT_FOUND)
   const req = {}
@@ -32,6 +32,51 @@ tape('dbErrorHandlers.custom with id not found error', t => {
   dbErrorHandlers.custom(testError, req, res)
 
   t.ok(resSpy.called, 'res.boom.notFound was called')
-  t.equal(resSpy.args[0][0], errMessages.GET_ID_NOT_FOUND, 'res.boom.notFound was called with the correct Error message')
+  t.equal(resSpy.args[0][0], errMessages.GET_ID_NOT_FOUND, 'res.boom.notFound was called with the correct error message')
+  t.end()
+})
+
+tape('dbErrorHandlers.custom with UPDATE_ID_NOT_FOUND', t => {
+  // construct arguments
+  const testError = createCustomDbError(errMessages.UPDATE_ID_NOT_FOUND)
+  const req = {}
+  const res = {boom: { badRequest: function () {} }}
+  const resSpy = sinon.spy(res.boom, 'badRequest')
+
+  // call the function we're testing
+  dbErrorHandlers.custom(testError, req, res)
+
+  t.ok(resSpy.called, 'res.boom.badRequest was called')
+  t.equal(resSpy.args[0][0], errMessages.UPDATE_ID_NOT_FOUND, 'res.boom.badRequest was called with the correct error message')
+  t.end()
+})
+
+tape('dbErrorHandlers.custom with DELETE_ID_NOT_FOUND', t => {
+  // construct arguments
+  const testError = createCustomDbError(errMessages.DELETE_ID_NOT_FOUND)
+  const req = {}
+  const res = {boom: { badRequest: function () {} }}
+  const resSpy = sinon.spy(res.boom, 'badRequest')
+
+  // call the function we're testing
+  dbErrorHandlers.custom(testError, req, res)
+
+  t.ok(resSpy.called, 'res.boom.badRequest was called')
+  t.equal(resSpy.args[0][0], errMessages.DELETE_ID_NOT_FOUND, 'res.boom.badRequest was called with the correct error message')
+  t.end()
+})
+
+tape('dbErrorHandlers.custom with UNHANDLED', t => {
+  // construct arguments
+  const testError = createCustomDbError('UNHANDLED')
+  const req = {}
+  const res = {boom: { badImplementation: function () {} }}
+  const resSpy = sinon.spy(res.boom, 'badImplementation')
+
+  // call the function we're testing
+  dbErrorHandlers.custom(testError, req, res)
+
+  t.ok(resSpy.called, 'res.boom.badImplementation was called')
+  t.equal(resSpy.args[0][0], errMessages.UNHANDLED_CUSTOM, 'res.boom.badImplementation was called with the correct error message')
   t.end()
 })
