921df2eaafbe8312f762e1a06daa5f4e2e2eea94
Matt Davis <matthewpeterdavis@gmail.com>
2017-07-27 13:48:32 +0300

    fix middleWare typo

--- tests/dbErrorHandlers.test.js
+++ tests/dbErrorHandlers.test.js
@@ -6,12 +6,12 @@ const tape = require('tape')
 
 tape('dbErrorHandlers.custom with non custom error', t => {
   // make function to call the middle and generate spies we need
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
   // make error to test with
   const notCustomError = new Error('Not a custom error')
   notCustomError.name = 'Not Custom'
   // extract the spy
-  const { nextSpy } = generateSpiesAndCallMiddleWare(notCustomError)
+  const { nextSpy } = generateSpiesAndCallMiddleware(notCustomError)
   // carry out the tests
   t.ok(nextSpy.called, `next was called`)
   t.equal(nextSpy.args[0][0], notCustomError, `next was called with the error`)
@@ -19,12 +19,12 @@ tape('dbErrorHandlers.custom with non custom error', t => {
 })
 
 tape('dbErrorHandlers.custom with GET_ID_NOT_FOUND', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
 
   const notFoundError = new Error(errMessages.GET_ID_NOT_FOUND)
   notFoundError.name = errNames.CUSTOM
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(notFoundError)
+  const { resSpy } = generateSpiesAndCallMiddleware(notFoundError)
 
   t.ok(resSpy.boom.notFound.called, `notFound method was called`)
   t.equal(resSpy.boom.notFound.args[0][0], notFoundError.message, `notFound was called with the error message`)
@@ -32,12 +32,12 @@ tape('dbErrorHandlers.custom with GET_ID_NOT_FOUND', t => {
 })
 
 tape('dbErrorHandlers.custom with UPDATE_ID_NOT_FOUND', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
 
   const badRequestError = new Error(errMessages.UPDATE_ID_NOT_FOUND)
   badRequestError.name = errNames.CUSTOM
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badRequestError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badRequestError)
 
   t.ok(resSpy.boom.badRequest.called, `badRequest method was called`)
   t.equal(resSpy.boom.badRequest.args[0][0], badRequestError.message, `badRequest was called with the error message`)
@@ -45,12 +45,12 @@ tape('dbErrorHandlers.custom with UPDATE_ID_NOT_FOUND', t => {
 })
 
 tape('dbErrorHandlers.custom with DELETE_ID_NOT_FOUND', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
 
   const badRequestError = new Error(errMessages.DELETE_ID_NOT_FOUND)
   badRequestError.name = errNames.CUSTOM
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badRequestError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badRequestError)
 
   t.ok(resSpy.boom.badRequest.called, `badRequest method was called`)
   t.equal(resSpy.boom.badRequest.args[0][0], badRequestError.message, `badRequest was called with the error message`)
@@ -58,12 +58,12 @@ tape('dbErrorHandlers.custom with DELETE_ID_NOT_FOUND', t => {
 })
 
 tape('dbErrorHandlers.custom with UNHANDLED', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.custom)
 
   const badImplementationError = new Error(errMessages.UNHANDLED_CUSTOM)
   badImplementationError.name = errNames.CUSTOM
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badImplementationError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badImplementationError)
 
   t.ok(resSpy.boom.badImplementation.called, `badImplementation method was called`)
   t.equal(resSpy.boom.badImplementation.args[0][0], badImplementationError.message, `badImplementation was called with the error message`)
@@ -71,12 +71,12 @@ tape('dbErrorHandlers.custom with UNHANDLED', t => {
 })
 
 tape('dbErrorHandlers.mongo with non mongo error', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongo)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongo)
 
   const notMongoError = new Error('Not a mongo error')
   notMongoError.name = 'Not Mongo'
 
-  const { nextSpy } = generateSpiesAndCallMiddleWare(notMongoError)
+  const { nextSpy } = generateSpiesAndCallMiddleware(notMongoError)
 
   t.ok(nextSpy.called, `next was called`)
   t.equal(nextSpy.args[0][0], notMongoError, `next was called with the error`)
@@ -84,13 +84,13 @@ tape('dbErrorHandlers.mongo with non mongo error', t => {
 })
 
 tape('dbErrorHandlers.mongo with MONGO_DUPLICATE_KEY', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongo)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongo)
 
   const badRequestError = new Error(errMessages.DUPLICATE_KEY)
   badRequestError.name = errNames.MONGO
   badRequestError.code = errCodes.MONGO_DUPLICATE_KEY
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badRequestError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badRequestError)
 
   t.ok(resSpy.boom.badRequest.called, `badRequest method was called`)
   t.equal(resSpy.boom.badRequest.args[0][0], badRequestError.message, `badRequest was called with the error message`)
@@ -98,12 +98,12 @@ tape('dbErrorHandlers.mongo with MONGO_DUPLICATE_KEY', t => {
 })
 
 tape('dbErrorHandlers.mongo with UNHANDLED', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongo)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongo)
 
   const badImplementationError = new Error(errMessages.UNHANDLED_MONGO)
   badImplementationError.name = errNames.MONGO
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badImplementationError, 'badImplementation')
+  const { resSpy } = generateSpiesAndCallMiddleware(badImplementationError, 'badImplementation')
 
   t.ok(resSpy.boom.badImplementation.called, `badImplementation method was called`)
   t.equal(resSpy.boom.badImplementation.args[0][0], badImplementationError.message, `badImplementation was called with the error message`)
@@ -111,12 +111,12 @@ tape('dbErrorHandlers.mongo with UNHANDLED', t => {
 })
 
 tape('dbErrorHandlers.mongoose with MONGOOSE_VALIDATION', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongoose)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongoose)
 
   const badRequestError = new Error(errMessages.VALIDATION_FAILED)
   badRequestError.name = errNames.MONGOOSE_VALIDATION
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badRequestError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badRequestError)
 
   t.ok(resSpy.boom.badRequest.called, `badRequest method was called`)
   t.equal(resSpy.boom.badRequest.args[0][0], badRequestError.message, `badRequest was called with the error message`)
@@ -124,12 +124,12 @@ tape('dbErrorHandlers.mongoose with MONGOOSE_VALIDATION', t => {
 })
 
 tape('dbErrorHandlers.mongoose with MONGOOSE_CAST', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongoose)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongoose)
 
   const badRequestError = new Error(errMessages.INVALID_ID)
   badRequestError.name = errNames.MONGOOSE_CAST
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badRequestError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badRequestError)
 
   t.ok(resSpy.boom.badRequest.called, `badRequest method was called`)
   t.equal(resSpy.boom.badRequest.args[0][0], badRequestError.message, `badRequest was called with the error message`)
@@ -137,12 +137,12 @@ tape('dbErrorHandlers.mongoose with MONGOOSE_CAST', t => {
 })
 
 tape('dbErrorHandlers.mongoose with UNHANDLED', t => {
-  const generateSpiesAndCallMiddleWare = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongoose)
+  const generateSpiesAndCallMiddleware = spyGeneratorErrorMiddlewareCaller(dbErrorHandlers.mongoose)
 
   const badImplementationError = new Error(errMessages.UNHANDLED_MONGOOSE)
   badImplementationError.name = 'UNHANDLED MONGOOSE'
 
-  const { resSpy } = generateSpiesAndCallMiddleWare(badImplementationError)
+  const { resSpy } = generateSpiesAndCallMiddleware(badImplementationError)
 
   t.ok(resSpy.boom.badImplementation.called, `badImplementation method was called`)
   t.equal(resSpy.boom.badImplementation.args[0][0], badImplementationError.message, `badImplementation was called with the error message`)
