38b70b30d72746aaacc61eb02864679ab6d9da51
Matt Lubel <matt_lubel@hotmail.com>
2017-08-13 15:59:44 +0300

    amend event controller to use new errorIfNotFound

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,12 +1,13 @@
 const Event = require('../models/Event')
+const { errorIfNotFound } = require('../db/utils')
+const { messages: errMessages } = require('../constants/errors')
 
 const eventController = module.exports = {}
 
 eventController.getAll = (req, res, next) => {
   // sends back array of events, filtered by queries
   Event.find(req.query)
-    .select('-__v -createdAt -updatedAt')
-    .populate('placeId', '-__v -createdAt -updatedAt')
+    .populate('placeId')
     .sort('startTime')
     .then(events => res.status(200).send(events))
     .catch(next)
@@ -16,7 +17,9 @@ eventController.getById = (req, res, next) => {
   // receives id in url
   // sends back one event or errors
   const id = req.params.id
-  Event.findByIdOrError(id)
+  const findEventById = Event.findById(id).populate('placeId')
+
+  errorIfNotFound(findEventById, errMessages.GET_ID_NOT_FOUND)
     .then(event => res.status(200).send(event))
     .catch(next)
 }
@@ -35,7 +38,9 @@ eventController.update = (req, res, next) => {
   // receives updated json for event in body
   // updates or errors
   const id = req.params.id
-  Event.findByIdAndUpdateOrError(id, req.body, { new: true })
+  const findEventByIdAndUpdate = Event.findByIdAndUpdate(id, req.body, { new: true })
+  
+  errorIfNotFound(findEventByIdAndUpdate, errMessages.UPDATE_ID_NOT_FOUND)
     .then(updatedEvent => res.status(200).send(updatedEvent))
     .catch(next)
 }
@@ -44,7 +49,8 @@ eventController.delete = (req, res, next) => {
   // receives id in url
   // deletes or errors
   const id = req.params.id
-  Event.findByIdAndRemoveOrError(id)
+
+  errorIfNotFound(Event.findByIdAndRemove(id), errMessages.DELETE_ID_NOT_FOUND)
     .then(() => res.status(204).send())
     .catch(next)
 }
