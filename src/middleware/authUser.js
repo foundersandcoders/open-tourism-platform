ffeff5fe19fff7fc9e101c12f01de5b32e268737
Matt Davis <matthewpeterdavis@gmail.com>
2017-08-15 17:26:01 +0100

    use boom with custom error message

--- src/middleware/authUser.js
+++ src/middleware/authUser.js
@@ -1,6 +1,8 @@
-const boom = require('express-boom')
+const boom = require('boom')
 
 const roles = require('../constants/roles.js')
+const { auth } = require('../constants/errors.json')
+
 const User = require('../models/User')
 
 const verifyRole = ({ minRole }) => user => {
@@ -10,13 +12,13 @@ const verifyRole = ({ minRole }) => user => {
   if (minRole === roles.ADMIN && userScope !== roles.BASIC) return
   if (minRole === roles.SUPER && userScope === roles.SUPER) return
 
-  return Promise.reject(boom.unauthorized())
+  return Promise.reject(boom.unauthorized(auth.UNAUTHORIZED))
 }
 
 const addUserIdToSession = req => {
   return User.find({ username: req.user.username }).then(users => {
     if (users.length === 0) {
-      return Promise.reject(boom.unauthorized())
+      return Promise.reject(boom.unauthorized(auth.UNAUTHORIZED))
     }
 
     req.user.id = users[0].id
