69921ac5afe24ea34c54f765bc6c23d35e7db2fe
Matt Lubel <matt_lubel@hotmail.com>
2017-08-15 12:12:46 +0300

    set up express-handlbars

--- src/server.js
+++ src/server.js
@@ -4,12 +4,24 @@ const authRouter = require('./routers/authRouter')
 const bodyParser = require('body-parser')
 const boom = require('express-boom')
 const path = require('path')
+const expressHandlebars  = require('express-handlebars')
 
 const customErrorHandler = require('./middleware/customErrorHandler')
 const mongoErrorHandler = require('./middleware/mongoErrorHandler')
 const mongooseErrorHandler = require('./middleware/mongooseErrorHandler')
 const finalErrorHandler = require('./middleware/finalErrorHandler')
+
+const expressHandlebarsConfig = {
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+}
+
 const server = express()
+
+server.engine('handlebars', expressHandlebars(expressHandlebarsConfig))
+server.set('view engine', 'handlebars')
+server.set('views', path.join(__dirname, 'views'))
 
 server.use(express.static(path.join(__dirname, 'public')))
 server.use(bodyParser.urlencoded({ extended: true }))
@@ -17,7 +29,7 @@ server.use(bodyParser.json({ extended: true }))
 server.use(boom())
 
 server.use(apiRouter)
-server.use('/oauth', authRouter)
+server.use(authRouter)
 
 server.use(customErrorHandler)
 server.use(mongoErrorHandler)
