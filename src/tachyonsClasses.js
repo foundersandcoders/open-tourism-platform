70bbbb1697df3d4dcb16844754a02dc174ba0a7d
Matt Lubel <matt_lubel@hotmail.com>
2017-08-16 15:47:18 +0300

    change style slightly

--- src/tachyonsClasses.js
+++ src/tachyonsClasses.js
@@ -3,6 +3,6 @@ module.exports = {
   form: "center w-90 w-60-ns pa3",
   formTitle: "tc",
   formLabel: "db center ma3",
-  formInput: "db w-100 pa1 ma1 br3",
+  formInput: "db w-100 pa1 mv1 br3",
   formSubmit: "db center bg-white br-pill pv2 ph3 ma4"
 }
