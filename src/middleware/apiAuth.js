78458da70c59ddd84ca6cb85f0c9fc12ebe79470
Matt Lubel <matt_lubel@hotmail.com>
2017-09-05 23:31:08 +0300

    a start of the implementation of apiAuth

--- src/middleware/apiAuth.js
+++ src/middleware/apiAuth.js
@@ -4,27 +4,26 @@ const { auth, messages: errMessages } = require('../constants/errors.json')
 const { rejectIfNull } = require('../db/utils')
 const { oauthServer } = require('../controllers/oauth')
 
-const checkUserRole =
-  ({ minSufficientRole, resource, resourceOwnerIsSufficient }) => req => {
+const checkUserIsPermitted =
+  ({ minSufficientRole, resource, resourceOwnerIsSufficient }) => (user, resourceId) => {
     const orderedRoles = [roles.BASIC, roles.ADMIN, roles.SUPER]
 
-  // throw error on initialisation if passed options are not valid
+    // throw error on initialisation if passed options are not valid
     if (!orderedRoles.includes(minSufficientRole)) {
       throw boom.badImplementation()
     }
-    if (resourceOwnerIsSufficient && (!resource || !req.params.id)) {
+    if (resourceOwnerIsSufficient && !resource) {
       throw boom.badImplementation()
     }
 
-    if (orderedRoles.indexOf(req.user.role) >= orderedRoles.indexOf(minSufficientRole)) {
+    if (orderedRoles.indexOf(user.role) >= orderedRoles.indexOf(minSufficientRole)) {
       return Promise.resolve()
     } else if (!resourceOwnerIsSufficient) {
       return Promise.reject(boom.unauthorized(auth.UNAUTHORIZED))
     } else {
-      const resourceId = req.params.id
       return resource.findById(resourceId)
       .then(rejectIfNull(errMessages.GET_ID_NOT_FOUND))
-      .then(doc => doc.user === req.user.id
+      .then(doc => doc.user === user.id
         ? Promise.resolve()
         : Promise.reject(boom.unauthorized(auth.UNAUTHORIZED))
       )
@@ -32,14 +31,8 @@ const checkUserRole =
   }
 
 module.exports = opts => (req, res, next) => {
-  
-  // oauthServer.authenticate()
-  // .then(token => {
-  //   // checkUserRole(opts)(req)
-  //   // .then(res => next())
-  //   // .catch(err => next(err))
-  // })
-  // .catch(err => next(err))
-
-  next()
+  oauthServer.authenticate()
+  .then(token => checkUserIsPermitted(opts)(token.user, req.params.id))
+  .then(() => next())
+  .catch(err => next(err))
 }
