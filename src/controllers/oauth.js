0966cf8982a9cf3ec4b45e71db515967177ce403
Matt Lubel <matt_lubel@hotmail.com>
2017-09-07 10:14:45 +0300

    export oauth server from oauth controller

--- src/controllers/oauth.js
+++ src/controllers/oauth.js
@@ -16,6 +16,8 @@ const oauth = new OAuthServer({ model: authModel })
 
 const oauthController = module.exports = {}
 
+oauthController.oauthServer = oauth
+
 oauthController.getAuthorizePage = (req, res, next) => {
   if (!req.query || !req.query.client_id) {
     return res.boom.badRequest(errMessages.NO_CLIENT_ID)
