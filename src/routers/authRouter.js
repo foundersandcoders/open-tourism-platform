e866face610e7f07ea91ef6d0e2dfc1a9a87ba5f
Matt Davis <matthewpeterdavis@gmail.com>
2017-09-12 15:41:41 +0300

    remove dummy route from auth route into a test

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,12 +1,7 @@
 const oauthController = require('../controllers/oauth')
 const sessionController = require('../controllers/session')
-const appsController = require('../controllers/apps')
 
 const validateJWT = require('../middleware/validateJWT.js')
-const validateUser = require('../middleware/validateUser.js')
-const checkRole = require('../middleware/rolePermission.js')
-
-const roles = require('../constants/roles.js')
 
 const router = require('express').Router()
 
@@ -27,14 +22,5 @@ router.route('/oauth/authorize')
 
 router.route('/oauth/token')
   .post(oauthController.getToken)
-
-// secure route with dummy handler for now
-router.route('/apps')
-  .get(
-    validateJWT(),
-    validateUser,
-    checkRole({ minRole: roles.SUPER }),
-    appsController.get
-  )
 
 module.exports = router
