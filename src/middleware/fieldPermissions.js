d094d064ed9a9c093581d663ccbd912595e76ba2
Matt Lubel <matt_lubel@hotmail.com>
2017-09-13 17:20:06 +0300

    fix for lint

--- src/middleware/fieldPermissions.js
+++ src/middleware/fieldPermissions.js
@@ -1,18 +1,16 @@
-const url = require('url')
 const boom = require('boom')
-const roles = require('../constants/roles.js')
 const { messages: errMessages } = require('../constants/errors.json')
 
 const { hasSufficientRole } = require('./permissions')
 
 module.exports = fieldsMinRoles => (req, res, next) => {
-  const permissionedFields = Object.keys(fieldMinRole)
+  const permissionedFields = Object.keys(fieldsMinRoles)
   const unauthorizedFields = Object.keys(req.body)
     // filter out fields which are not permissioned or not in the schema
     .filter(field => permissionedFields.includes(field))
     // filter down to fields which user is not permitted to write to
     .filter(field =>
-      !hasSufficientRole({ minSufficientRole: fieldMinRoles[field] })(req.user))
+      !hasSufficientRole({ minSufficientRole: fieldsMinRoles[field] })(req.user))
 
   if (unauthorizedFields.length > 0) {
     const message = errMessages.FIELD_UNAUTHORIZED + unauthorizedFields.join(', ')
@@ -21,6 +19,3 @@ module.exports = fieldsMinRoles => (req, res, next) => {
 
   next()
 }
-
-
-
--- tests/middleware/fieldPermissions.test.js
+++ tests/middleware/fieldPermissions.test.js
@@ -5,30 +5,30 @@ const server = require('../../src/server')
 const fieldPermissions = require('../../src/middleware/fieldPermissions.js')
 const roles = require('../../src/constants/roles.js')
 
-const User = require('../../src/models/User')
-const Event = require('../../src/models/Event')
+// const User = require('../../src/models/User')
+// const Event = require('../../src/models/Event')
 
-const { user } = require('../fixtures/users.json')
-const { event } = require('../fixtures/events.json')
-const wrongUser = { id: 'wrong' }
+// const { user } = require('../fixtures/users.json')
+// const { event } = require('../fixtures/events.json')
+// const wrongUser = { id: 'wrong' }
 
 // prepare the db
-tape('emptying db.', t => {
-  Promise.all([
-    User.remove({}),
-    Event.remove({})
-  ])
-  .then(() => t.end())
-  .catch(err => t.end(err))
-})
+// tape('emptying db.', t => {
+//   Promise.all([
+//     User.remove({}),
+//     Event.remove({})
+//   ])
+//   .then(() => t.end())
+//   .catch(err => t.end(err))
+// })
 
-tape('filling db.', t => {
-  Promise.all([
-    User.create(user),
-    Event.create(event)
-  ])
-  .then(() => t.end())
-  .catch(err => t.end(err))
-})
+// tape('filling db.', t => {
+//   Promise.all([
+//     User.create(user),
+//     Event.create(event)
+//   ])
+//   .then(() => t.end())
+//   .catch(err => t.end(err))
+// })
 
 
