ab07101513dd7f825e95784744f7ee911591b5a2
Matt Lubel <matt_lubel@hotmail.com>
2017-09-17 15:01:01 +0300

    move middleware into testable function

--- src/middleware/fieldPermissions.js
+++ src/middleware/fieldPermissions.js
@@ -3,17 +3,27 @@ const { messages: errMessages } = require('../constants/errors.json')
 
 const { hasSufficientRole } = require('./permissions')
 
-module.exports = fieldsPermissions => (req, res, next) => {
-  const permissionedFields = Object.keys(fieldsPermissions)
-  const unauthorizedFields = Object.keys(req.body)
-    .filter(field => permissionedFields.includes(field))
+// const checkFieldPermission = permissionedFields => field =>
+
+const getUnauthorizedFields = fieldToPermissionsMapping => fieldsToChange => user => {
+  const permissionedFields = Object.keys(fieldToPermissionsMapping)
+  return fieldsToChange
     // filter out fields which are not permissioned
-    .filter(field => {
-      const { minRole, ownerIsPermitted } = fieldsPermissions[field]
+    .filter(field => permissionedFields.includes(field))
       // filter down to fields which user is not permitted to write to
-      return !hasSufficientRole({ minRole })(req.user) &&
-        !(ownerIsPermitted && req.user && req.user.isResourceOwner)
+    .filter(field => {
+      const { minRole, ownerIsPermitted } = fieldToPermissionsMapping[field]
+      return !hasSufficientRole({ minRole })(user) &&
+        !(ownerIsPermitted && user && user.isResourceOwner)
     })
+}
+
+module.exports = fieldToPermissionsMapping => (req, res, next) => {
+  const permissionedFields = Object.keys(fieldToPermissionsMapping)
+  const fieldsToChange = Object.keys(req.body)
+
+  const unauthorizedFields =
+    getUnauthorizedFields(fieldToPermissionsMapping)(fieldsToChange)(req.user)
 
   if (unauthorizedFields.length > 0) {
     const message = errMessages.FIELD_UNAUTHORIZED + unauthorizedFields.join(', ')
@@ -22,3 +32,5 @@ module.exports = fieldsPermissions => (req, res, next) => {
 
   next()
 }
+
+module.exports.getUnauthorizedFields = getUnauthorizedFields
