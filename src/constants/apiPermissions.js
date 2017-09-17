c12ed7f734aaef778a05a3f72c158f3426a2f61b
Matt Lubel <matt_lubel@hotmail.com>
2017-09-17 13:25:16 +0300

    add permissions constants file

---
+++ src/constants/apiPermissions.js
@@ -0,0 +1,18 @@
+const roles = require('./roles')
+
+module.exports = {
+	User: {
+    put: {
+      minRole: roles.ADMIN,
+      ownerIsPermitted: true
+    },
+    delete: {
+      minRole: roles.SUPER,
+      ownerIsPermitted: false
+    },
+    fields: {
+      _id: { minRole: roles.SUPER },
+      username: { minRole: roles.SUPER, ownerIsPermitted: true }
+    }
+  }
+}
