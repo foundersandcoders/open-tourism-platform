572bb223c0484105e06c8e1ffff69b8d1f6fa809
Matt Lubel <matt_lubel@hotmail.com>
2017-09-17 15:01:16 +0300

    add a test for middleware

--- tests/middleware/fieldPermissions.test.js
+++ tests/middleware/fieldPermissions.test.js
@@ -1,33 +1,46 @@
-// const tape = require('tape')
-// const supertest = require('supertest')
-// const server = require('../../src/server')
-// const validateHeaderToken = require('../../src/middleware/validateHeaderToken')
-// const permissions = require('../../src/middleware/permissions')
-// const fieldPermissions = require('../../src/middleware/fieldPermissions.js')
-// const roles = require('../../src/constants/roles.js')
+const tape = require('tape')
+const supertest = require('supertest')
+const server = require('../../src/server')
 
-// const User = require('../../src/models/User')
-// const Event = require('../../src/models/Event')
+const { getUnauthorizedFields } = require('../../src/middleware/fieldPermissions.js')
+const roles = require('../../src/constants/roles.js')
 
-// const { user } = require('../fixtures/users.json')
-// const { event } = require('../fixtures/events.json')
-// const wrongUser = { id: 'wrong' }
+const User = require('../../src/models/User')
+const Event = require('../../src/models/Event')
+
+const { user } = require('../fixtures/users.json')
+const { event } = require('../fixtures/events.json')
+const wrongUser = { id: 'wrong' }
 
 // prepare the db
-// tape('emptying db.', t => {
-//   Promise.all([
-//     User.remove({}),
-//     Event.remove({})
-//   ])
-//   .then(() => t.end())
-//   .catch(err => t.end(err))
-// })
+tape('emptying db.', t => {
+  Promise.all([
+    User.remove({}),
+    Event.remove({})
+  ])
+  .then(() => t.end())
+  .catch(err => t.end(err))
+})
 
-// tape('filling db.', t => {
-//   Promise.all([
-//     User.create(user),
-//     Event.create(event)
-//   ])
-//   .then(() => t.end())
-//   .catch(err => t.end(err))
-// })
+tape('filling db.', t => {
+  Promise.all([
+    User.create(user),
+    Event.create(event)
+  ])
+  .then(() => t.end())
+  .catch(err => t.end(err))
+})
+
+// the tests
+tape('test getUnauthorizedFields', t => {
+  const user = {}
+  const fieldPermissions = {}
+  const fieldsToChange = []
+
+  const unauthorizedFields =
+    getUnauthorizedFields(fieldPermissions)(fieldsToChange)(user)
+
+  t.equal(unauthorizedFields.length, 0, 'no unauthorized fields')
+  t.end()
+})
+
