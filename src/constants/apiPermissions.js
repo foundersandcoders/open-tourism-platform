df32bf88d08d94db388fe41225de8796434f415d
Matt Lubel <matt_lubel@hotmail.com>
2017-09-17 17:38:22 +0300

    add permissions constants #150

--- src/constants/apiPermissions.js
+++ src/constants/apiPermissions.js
@@ -2,10 +2,20 @@ const roles = require('./roles')
 
 module.exports = {
   User: {
-    
+
   },
   Event: {
-
+    update: {
+      minRole: roles.ADMIN
+      ownerIsPermitted: true  
+    },
+    delete: {
+      minRole: roles.SUPER
+    },
+    fields: {
+      _id: { minRole: roles.SUPER },
+      owner: { minRole: roles.ADMIN }
+    }
   },
   Product: {
 
