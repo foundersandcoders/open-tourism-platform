04d4ec6860f333180422d6b949e502d9b8cdec3a
mario <mario.saliba98@gmail.com>
2018-03-16 18:09:53 +0200

    extract emails of users through the mongoose query rel #187

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -3,36 +3,24 @@ const User = require('../models/User')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 sgMail.setSubstitutionWrappers('{{', '}}')
 
-const sendEmails = (emails, content) =>
-  new Promise((resolve, reject) => {
-    const msg = {
-      to: emails,
-      from: 'opentourismplatform@gmail.com',
-      templateId: '0839aec2-dd34-459d-9493-4fd9682b9073',
-      substitutions: {
-        name: content.en.name,
-        id: content._id
-      }
+const sendEmails = (emails, content) => {
+  const msg = {
+    to: emails,
+    from: 'opentourismplatform@gmail.com',
+    templateId: '0839aec2-dd34-459d-9493-4fd9682b9073',
+    substitutions: {
+      name: content.en.name,
+      id: content._id
     }
+  }
 
-    sgMail
-      .sendMultiple(msg)
-      .then(resolve)
-      .catch(reject)
-  })
-
-const extractEmails = users =>
-  users.reduce((acc, curr) => {
-    if (curr.role !== 'BASIC') {
-      return acc.concat(curr.email)
-    }
-    return acc
-  }, [])
+  sgMail
+    .sendMultiple(msg)
+}
 
 module.exports = content =>
   new Promise((resolve, reject) =>
-    User.find()
-      .then(extractEmails)
-      .then((emails) => sendEmails(emails, content))
+    User.find({ role: { $not: /BASIC/ } }, 'email')
+      .then(emails => sendEmails(emails, content))
       .then(() => resolve(content))
       .catch(reject))
